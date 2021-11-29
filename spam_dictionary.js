17011c07a57e4261962731cf732266d18026636e
chatoxz <carrerasssebastian@gmail.com>
2021-11-29 17:09:18 -0300

    Create spam_dictionary.js

---
+++ spam_dictionary.js
@@ -0,0 +1,29 @@
+var spam_dictionary = [
+    'sugar','Sugar',
+    ' babe ',' baby ',
+    'caregiver', 'daddy',
+    'allowance','Allowance',
+    'spoil ','Spoil ',
+    'mistres','goddess','Mistres','Goddess',
+    'slut ',' dom ',
+
+    'evil bloom','evil Bloom','TEVIL BLOOM',
+
+    'slave','Slave','feminize',
+
+    'itcoin','bit coin',
+
+    'adult game',
+    'ay Ban','ayBan','ay ban',
+    'glaai','rbcnd','glbe','glxllv','18sexy','okgr','okec','linoduba','okkl','linodps','ataxa88','linodxz','okgx',
+    'spun ','methamphetamine',' meth ','blowing clouds','spun clouds','crystal meth','shards crystal','shard slammers','slammed','smokeweeedeveryday','weedlife',
+    'docs.google.com/drawings',
+
+    // T&S issues
+    ' ana ',' mia ', ' Thinspo','Bonespo','Meanspo ','CW','GW','UGW',' bone','hookup',
+
+    //ICIM
+    'ddlg',
+    //TESTING
+    'Holis',
+];
